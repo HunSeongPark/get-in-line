b457a3657627f6b7d5a0918de48145e5add3b5c0
Hunseong <phunseong@gmail.com>
2022-05-02 04:10:50 +0900

    rename : BaseAPIController -> BaseController

--- src/main/java/com/hunseong/corona/controller/BaseController.java
+++ src/main/java/com/hunseong/corona/controller/BaseController.java
@@ -1,4 +1,4 @@
-package com.hunseong.corona.controller.api;
+package com.hunseong.corona.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * Created by Hunseong on 2022/05/02
  */
 @Controller
-public class APIBaseController {
+public class BaseController {
 
     @GetMapping("/")
     public String root() {
